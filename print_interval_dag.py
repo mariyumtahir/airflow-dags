b4a55d5a5582015ec70f2ba157550a69357c9663
mariyumtahir <whenadoctorwrites@gmail.com>
2026-05-05 17:21:29 +0500

    saare macros print karne ke liye update

--- print_interval_dag.py
+++ print_interval_dag.py
@@ -1,19 +1,17 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.providers.standard.operators.python import PythonOperator 
 from datetime import datetime
 
 def print_interval(**context):
-    start = context["data_interval_start"]
-    end   = context["data_interval_end"]
-    print(f"Interval Start: {start}")
-    print(f"Interval End:   {end}")
+    print("=== ALL AVAILABLE MACROS ===")
+    for key, value in context.items():
+        print(f"{key}: {value}")
 
 with DAG(
     dag_id="print_interval_dag",
     start_date=datetime(2025, 5, 1),
     schedule="0 */3 * * *",
     catchup=True,
-    tags=["example"],
 ) as dag:
 
     print_task = PythonOperator(
